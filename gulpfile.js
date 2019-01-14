ca3e5a96d0e29ee4bf3e0e7f240c9ce9a84d8137
Szau <Szalkowski.Krzysztof.Z@gmail.com>
2019-01-15 00:18:08 +0100

    gulp

--- gulpfile.js
+++ gulpfile.js
@@ -2,3 +2,26 @@ var install = require("gulp-install");
  
 gulp.src(['./bower.json', './package.json'])
   .pipe(install());
+
+gulp.task('analyze', function() {
+    log('Analyzing code with JSHint and JSCS.');
+    return gulp
+        .src('./src/scripts') // glob pattern for all of my JS
+
+        // utilizes gulp-if and gulp-print to check for cmd args
+        .pipe(plugins.if(args.verbose, plugins.print()))
+
+        // utilizes gulp-jscs to inspect JS
+        .pipe(plugins.jscs())
+
+        // utilizes gulp-jshint to inspect JS
+        .pipe(plugins.jshint())
+
+        // utilizes jshint-stylish to show the results
+        .pipe(plugins.jshint.reporter('jshint-stylish'));
+});
+
+function log(msg) {
+   // utilizes gulp-util's log function
+    plugins.util.log(plugins.util.colors.blue(msg));
+}
